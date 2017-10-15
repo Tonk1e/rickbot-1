72fe1e3db2e206c703b1774423ca4009d4767ef3
Euab <euanmills26@gmail.com>
2017-10-15 12:30:48 +0100

    New badass plugin storage system!

--- bot/database.py
+++ bot/database.py
@@ -1,6 +1,7 @@
 import redis
 import asyncio
 import logging
+from storage import Storage
 
 log = logging.getLogger('discord')
 
@@ -8,5 +9,17 @@ class Db(object):
 
     def __init__(self, redis_url):
         self.redis_url = redis_url
-        self.redis = redis.StrictRedis.from_url(redis_url,
-                                                decode_responses=True)
+        self.redis = redis.Redis_from_url(redis_url, decode_responses=True)
+
+    def get_storage(self, plugin, server):
+        namespace = "storage:{}:{}:".format(
+            plugin.__class__.__name__,
+            server.id
+        )
+        storage = Storage.from_url(
+            self.redis_url,
+            decode_responses=True,
+            namespace=namespace
+        )
+
+        return storage
