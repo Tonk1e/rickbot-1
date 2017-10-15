a43b5376a9b0355c1f87faf9b15610a99df6070e
Euab <euanmills26@gmail.com>
2017-10-15 17:57:39 +0100

    Added RICKBOT_DEBUG enviroment variable

--- bot/bot.py
+++ bot/bot.py
@@ -6,6 +6,10 @@ logging.basicConfig(level=logging.INFO)
 
 token = os.getenv('RICKBOT_TOKEN')
 redis_url = os.getenv('REDIS_URL')
+rickbot_debug = os.getenv('RICKBOT_DEBUG')
+
+if rickbot_debug:
+    logging.basicConfig(level=logging.DEBUG)
 
 bot = RickBot(redis_url=redis_url)
 bot.run(token)
