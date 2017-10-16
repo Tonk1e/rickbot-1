ec12c7d078f05eae9f8024037411568ca9d92577
Euab <euanmills26@gmail.com>
2017-10-16 11:58:22 +0100

    Added the help plugin

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -3,6 +3,19 @@ import asyncio
 
 class Commands(Plugin):
 
+    dank_name = 'Custom Commands'
+
+    def get_commands(self, server):
+        storage = self.get_storage(server)
+        commands = sorted(storage.smembers('commands'))
+        cmds = []
+        for command in commands:
+            cmd = {
+                'name': command
+            }
+            cmds.append(cmd)
+        return cmds
+
     async def on_message(self, message):
         storage = self.get_storage(message.server)
         commands = storage.smembers('commands')
