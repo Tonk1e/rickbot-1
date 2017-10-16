c2305c7364a5975de05d6135905873811fd7c9ad
Euab <euanmills26@gmail.com>
2017-10-16 17:03:01 +0100

    minor

--- website/app.py
+++ website/app.py
@@ -10,12 +10,12 @@ import re
 
 app = Flask(__name__)
 app.debug = True
-app.config['SECRET_KEY'] = "\x10\xdf\xba\xed\xe5Ih\x17U\nQb~\x99\x01"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "\x10\xdf\xba\xed\xe5Ih\x17U\nQb~\x99\x01")
 
 REDIS_URL = os.environ.get('REDIS_URL')
 OAUTH2_CLIENT_ID = os.environ['OAUTH2_CLIENT_ID']
 OAUTH2_CLIENT_SECRET = os.environ['OAUTH2_CLIENT_SECRET']
-OAUTH2_REDIRECT_URI = 'http://localhost:5000/confirm_login'
+OAUTH2_REDIRECT_URI = os.environ.get('OAUTH2_REDIRECT_URI', 'http://localhost:5000/confirm_login')
 API_BASE_URL = os.environ.get('API_BASE_URL', 'https://discordapp.com/api')
 AUTHORIZATION_BASE_URL = API_BASE_URL + '/oauth2/authorize'
 TOKEN_URL = API_BASE_URL + '/oauth2/token'
