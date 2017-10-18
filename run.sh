39ba23777a1704a5766c854422f4f5e2cf946f40
Euab <euanmills26@gmail.com>
2017-10-18 18:53:16 +0100

    bump

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-docker stop rickbot && docker rm rickbot && docker run --name rickbot -d --restart="always" --env-file envfile.list --link redis:redis rickbot-bot && docker stop rickbot-web && docker rm rickbot-web && docker run -d --restart="always" --name rickbot-web --link redis:redis --env-file envfile.list rickbot-web
+docker stop rickbot && docker rm rickbot
+docker run --name rickbot -d --restart="always" --env-file envfile.list --link redis:redis rickbot-bot
+docker stop rickbot-web && docker rm rickbot-web
+docker run -d --restart="always" --name rickbot-web --link redis:redis --env-file envfile.list rickbot-web
